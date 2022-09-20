5c082007d27beea87dd0f68fec7ba2c3de48b5b0
Dieguerson <voices113@hotmail.com>
2022-09-20 20:18:21 -0300

    Ejercicio 18 fixes

--- Ejercicios/Ejercicio18/https_client/index.js
+++ Ejercicios/Ejercicio18/https_client/index.js
@@ -1,6 +1,21 @@
 const axios = require('axios')
+const { faker } = require('@faker-js/faker')
+
+const fakeProd = {
+  title: faker.commerce.product(),
+  price: faker.commerce.price(),
+  thumbnail: faker.image.imageUrl(640, 480, true),
+}
+
 
 axios.get('http://localhost:8080/api/productos')
   .then(res => {
     console.log(res.data)
   })
+  .catch(err => console.log(err))
+
+axios.post('http://localhost:8080/api/productos', {fakeProd})
+  .then(res => {
+    console.log(res.data)
+  })
+  .catch(err => console.log(err))
--- Ejercicios/Ejercicio18/src/routes/products.js
+++ Ejercicios/Ejercicio18/src/routes/products.js
@@ -2,9 +2,15 @@ const express = require('express');
 const { Router } = express;
 let router = new Router();
 
-const { fetchProducts } = require('../controllers/productsController');
+const { fetchProducts, saveNewProduct } = require('../controllers/productsController');
 
 router.get('/productos', async (_, res) => {
+  let products = await fetchProducts()
+  res.send(products);
+})
+
+router.post('/productos', async (req, res) => {
+  await saveNewProduct(req.body.fakeProd)
   let products = await fetchProducts()
   res.send(products);
 })
